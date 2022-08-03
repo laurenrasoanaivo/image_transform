195c68f6f8e1c5369bd7242cdd3ac99dd12d0dea
laurenrasoanaivo <hei.lauren.3@gmail.com>
2022-08-03 23:46:41 +0300

    Initial commit

---
+++ src/main/java/com/example/image_transform/ImageTransformApplication.java
@@ -0,0 +1,13 @@
+package com.example.image_transform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImageTransformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImageTransformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/image_transform/ImageTransformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.image_transform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImageTransformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
